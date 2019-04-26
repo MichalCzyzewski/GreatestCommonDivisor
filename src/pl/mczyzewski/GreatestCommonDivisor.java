66ca9da6d3bbedd6af04287d9df48d08513a82c3
Michal Czyzewski <czy.mich@gmail.com>
2019-04-26 13:40:05 +0200

    getGreatestCommonDivisor with two int

--- src/pl/mczyzewski/GreatestCommonDivisor.java
+++ src/pl/mczyzewski/GreatestCommonDivisor.java
@@ -2,4 +2,41 @@ package pl.mczyzewski;
 
 public class GreatestCommonDivisor
 {
+
+    public static int getGreatestCommonDivisor(int first, int second)
+    {
+
+        if (first < 10 || second < 10)
+        {
+            return -1;
+        }
+
+        int greComDivFir = 0;
+        int greComDivSec = 0;
+        int x=0;
+
+        while (x <=first)
+        {
+         greComDivFir = first % x;
+         if (greComDivFir == 0)
+         {
+             return greComDivFir;
+         }
+         x++;
+        }
+
+        while (x <=second)
+        {
+            greComDivFir = second % x;
+
+            if (greComDivFir == 0)
+            {
+                return greComDivSec;
+            }
+            x++;
+        }
+
+return greComDivFir;
+    }
+
 }
--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -3,6 +3,7 @@ package pl.mczyzewski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
     }
 }
